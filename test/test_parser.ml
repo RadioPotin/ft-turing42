f12f3352921cb441e3b0c34a1216e7cd42984034
Dario Pinto <laradiopotin@gmail.com>
2021-11-18 09:37:03 +0100

    more coverage for parser

--- test/test_parser.ml
+++ test/test_parser.ml
@@ -21,16 +21,19 @@ let test_files =
   ; "INVALID_TYPE_LIST_ASSOC.json"
   ; "INVALID_TYPE_ASSOC_INT.json"
   ; "INVALID_TYPE_ASSOC_LIST.json"
-  ; "INVALID_TYPE_TRANSITION_INT.json"
   ; "INVALID_TYPE_TRANSITION_ASSOC.json"
   ; "INVALID_TYPE_TRANSITION_ASSOC_INT.json"
   ; "INVALID_TYPE_TRANSITION_ASSOC_LIST.json"
+  ; "INVALID_DUPLICATE_STATE_DECLARATION.json"
   ; "INVALID_DUPLICATE_TRANSITION.json"
-  ; "INVALID_DUPLICATE_STATE.json"
+  ; "INVALID_TOSTATE_FIELD.json"
   ; "INVALID_FINAL_STATE.json"
   ; "INVALID_ALPHABET.json"
   ; "NOSUCHFILE.json"
   ]
+
+let non_assoc_json =
+  [ "INVALID_TYPE_MACHINE_INT.json"; "INVALID_TYPE_MACHINE_LIST.json" ]
 
 let test_parser () =
   if Sys.os_type = "Win32" || Sys.os_type = "Cygwin" then
@@ -40,4 +43,15 @@ let test_parser () =
     comment fmt "\nSTARTING PARSING TESTS **************";
     List.iter
       (fun machinename -> run_test fmt ("test_machines/" ^ machinename))
-      test_files
+      test_files;
+
+    List.iter
+      (fun machinename ->
+        Format.fprintf fmt "@.FILE: %s@." (Filename.basename machinename);
+        match
+          Lang.to_transitions_tbl [ "" ] "jsonfile"
+            (Lang.convert_json ("test_machines/" ^ machinename))
+        with
+        | exception Utils.Error -> ()
+        | _any -> assert false )
+      non_assoc_json
